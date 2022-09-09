43b28c973d77811d0f7b8b857738cd3082b3062c
epiii <gue.elfrianto@gmail.com>
2022-09-09 20:11:06 +0700

    fix warning issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import { User } from './features/user/User';
 
